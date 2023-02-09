4a4dad01424788d02d73fed447d69b82fdf0580d
TimWeberRE100 <Timothy.Weber@anu.edu.au>
2023-02-09 12:05:17 +1100

    Fixed single pits for screening

--- src/screening.cpp
+++ src/screening.cpp
@@ -717,7 +717,7 @@ int main(int nargs, char **argv) {
 	
 	for(ExistingReservoir r : existing_reservoirs){
       RoughBfieldReservoir reservoir = existing_reservoir_to_rough_reservoir(r);
-      reservoir.pit = search_config.search_type == SearchType::BULK_PIT;
+      reservoir.pit = (search_config.search_type == SearchType::BULK_PIT || search_config.search_type == SearchType::SINGLE_PIT);
       write_rough_reservoir_csv(csv_file, reservoir);
       write_rough_reservoir_data(csv_data_file, &reservoir);
     }
