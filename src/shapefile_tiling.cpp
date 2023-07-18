bb74c3bd2d71c7548e1f3a9fb9759ed578699daa
TimWeberRE100 <timothy.weber@anu.edu.au>
2023-07-18 19:56:07 +1000

    Load one shapefile at a time into memory

--- src/shapefile_tiling.cpp
+++ src/shapefile_tiling.cpp
@@ -35,38 +35,11 @@ struct Shape {
   int elevation = 0;
 };
 
-int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    std::cout << "Please specify FILTER, RIVER, or BLUEFIELD." << std::endl;
-    exit(1);
-  }
-  std::string type = std::string(argv[1]);
-  if (type != "FILTER" && type != "BLUEFIELD" && type != "RIVER") {
-    std::cout << "Please specify FILTER, RIVER, or BLUEFIELD." << std::endl;
-    exit(1);
-  }
-  std::cout << "Tiling started for " << type << std::endl;
-  printf("Tiling started\n");
+std::vector<Shape> read_shapefile(string filename, Model<bool> *to_keep, Model<vector<int>> *relevant_polygons, std::string type, int &SHAPE_TYPE){
+	vector<Shape> shapes;
+  	int iterator = 0;
 
-  parse_variables(convert_string("storage_location"));
-  parse_variables(convert_string(file_storage_location + "variables"));
-  unsigned long start_usec = walltime_usec();
-
-  vector<GridSquare> tasklist = read_tasklist(convert_string(file_storage_location + tasks_file));
-
-  Model<vector<int>> *relevant_polygons = new Model<vector<int>>(180, 360);
-  Model<bool> *to_keep = new Model<bool>(180, 360, MODEL_SET_ZERO);
-  vector<Shape> shapes;
-  int iterator = 0;
-
-  vector<string> shapefile_names;
-  if (type == "FILTER")
-    shapefile_names = filter_filenames_to_tile;
-  else
-    shapefile_names.push_back("input/existing_reservoirs/" + existing_reservoirs_shp);
-
-  for (string filename : shapefile_names) {
-    SHPHandle SHP = SHPOpen(convert_string(file_storage_location + filename), "rb");
+	SHPHandle SHP = SHPOpen(convert_string(file_storage_location + filename), "rb");
     DBFHandle DBF = DBFOpen(convert_string(file_storage_location + filename), "rb");
     int dbf_field = 0;
     int dbf_name_field = 0;
@@ -152,7 +125,72 @@ int main(int argc, char *argv[]) {
       printf("%d Polygons imported from %s\n", iterator, convert_string(filename));
     }
     SHPClose(SHP);
+	DBFClose(DBF);
+
+	return shapes;
+}
+
+void DBFCopy(DBFHandle &oldDBFHandle, DBFHandle &newDBFHandle) {
+    int numFields = DBFGetFieldCount(oldDBFHandle);
+    int numRecords = DBFGetRecordCount(oldDBFHandle);
+
+    for (int i = 0; i < numFields; i++) {
+        char fieldName[12];
+        int fieldWidth, fieldDecimals;
+        DBFFieldType fieldType = DBFGetFieldInfo(oldDBFHandle, i, fieldName, &fieldWidth, &fieldDecimals);
+        DBFAddField(newDBFHandle, fieldName, fieldType, fieldWidth, fieldDecimals);
+    }
+
+    for (int i = 0; i < numRecords; i++) {
+        for (int j = 0; j < numFields; j++) {
+            if (DBFIsAttributeNULL(oldDBFHandle, i, j)) {
+                DBFWriteNULLAttribute(newDBFHandle, i, j);
+            } else if (DBFGetFieldInfo(oldDBFHandle, j, nullptr, nullptr, nullptr) == FTString) {
+                const char* value = DBFReadStringAttribute(oldDBFHandle, i, j);
+                DBFWriteStringAttribute(newDBFHandle, i, j, value);
+            } else if (DBFGetFieldInfo(oldDBFHandle, j, nullptr, nullptr, nullptr) == FTInteger) {
+                int value = DBFReadIntegerAttribute(oldDBFHandle, i, j);
+                DBFWriteIntegerAttribute(newDBFHandle, i, j, value);
+            } else if (DBFGetFieldInfo(oldDBFHandle, j, nullptr, nullptr, nullptr) == FTDouble) {
+                double value = DBFReadDoubleAttribute(oldDBFHandle, i, j);
+                DBFWriteDoubleAttribute(newDBFHandle, i, j, value);
+            } else if (DBFGetFieldInfo(oldDBFHandle, j, nullptr, nullptr, nullptr) == FTLogical) {
+                const char* value = DBFReadLogicalAttribute(oldDBFHandle, i, j);
+                DBFWriteLogicalAttribute(newDBFHandle, i, j, *value);
+            }
+        }
+    }
+	return;
+}
+
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cout << "Please specify FILTER, RIVER, or BLUEFIELD." << std::endl;
+    exit(1);
   }
+  std::string type = std::string(argv[1]);
+  if (type != "FILTER" && type != "BLUEFIELD" && type != "RIVER") {
+    std::cout << "Please specify FILTER, RIVER, or BLUEFIELD." << std::endl;
+    exit(1);
+  }
+  std::cout << "Tiling started for " << type << std::endl;
+  printf("Tiling started\n");
+
+  parse_variables(convert_string("storage_location"));
+  parse_variables(convert_string(file_storage_location + "variables"));
+  unsigned long start_usec = walltime_usec();
+
+  vector<GridSquare> tasklist = read_tasklist(convert_string(file_storage_location + tasks_file));
+
+  Model<vector<int>> *relevant_polygons = new Model<vector<int>>(180, 360);
+  Model<bool> *to_keep = new Model<bool>(180, 360, MODEL_SET_ZERO);
+
+  vector<string> shapefile_names;
+  if (type == "FILTER")
+    shapefile_names = filter_filenames_to_tile;
+  else
+    shapefile_names.push_back("input/existing_reservoirs/" + existing_reservoirs_shp);    
 
   string output_location = file_storage_location + "input/shapefile_tiles";
   if(type=="RIVER")
@@ -161,61 +199,91 @@ int main(int argc, char *argv[]) {
     output_location = file_storage_location + "input/bluefield_shapefile_tiles";
   mkdir(convert_string(output_location), 0777);
   int z = 0;
-  for (GridSquare gs : tasklist) {
-    z++;
-    int lat = gs.lat;
-    int lon = gs.lon;
-    SHPHandle SHP = SHPCreate(
-        convert_string(output_location + "/" + str(gs) + "_shapefile_tile.shp"), SHAPE_TYPE);
-    DBFHandle DBF = DBFCreate(convert_string(output_location + "/" + str(gs) + "_shapefile_tile.dbf"));
-    int field_num=0, elevation_field_num=0, name_field_num =0;
-    if(type=="RIVER")
-      field_num = DBFAddField(DBF, convert_string("DIS_AV_CMS"), FTDouble, 10, 3);
-    if(type=="BLUEFIELD"){
-      field_num = DBFAddField(DBF, convert_string("Vol_total"), FTDouble, 10, 3);
-      elevation_field_num = DBFAddField(DBF, convert_string("Elevation"), FTInteger, 10, 0);
-      name_field_num = DBFAddField(DBF, convert_string("Lake_name"), FTString, 64, 0);
-    }
-    if (SHP == NULL) {
-      printf("Unable to create:%s\n",
-             convert_string(output_location + "/" + str(gs) + "_shapefile_tile.shp"));
-      exit(1);
-    }
+  bool first_file = true;
 
-    for (uint ipolygon = 0; ipolygon < relevant_polygons->get(lat + 90, lon + 180).size();
-         ipolygon++) {
-      Shape shape = shapes[relevant_polygons->get(lat + 90, lon + 180)[ipolygon]];
-      int nVertices = shape.points.size();
-      int panParts[1] = {0};
+  for (string filename : shapefile_names) {
+	std::vector<Shape> shapes = read_shapefile(filename,to_keep,relevant_polygons,type,SHAPE_TYPE);
 
-      double *padfX = new double[nVertices];
-      double *padfY = new double[nVertices];
-      double *padfZ = NULL, *padfM = NULL;
+	for (GridSquare gs : tasklist) {
+		z++;
+		int lat = gs.lat;
+		int lon = gs.lon;
+		
+		string shpName = output_location + "/" + str(gs) + "_shapefile_tile.shp";
+    	string dbfName = output_location + "/" + str(gs) + "_shapefile_tile.dbf";
 
-      for (int i = 0; i < nVertices; i++) {
-        GeographicCoordinate temp =
-            shape.points[i];
-        padfX[i] = temp.lon;
-        padfY[i] = temp.lat;
-      }
-      SHPObject *psObject;
-      psObject = SHPCreateObject(SHAPE_TYPE, -1, 0, panParts, NULL, nVertices, padfX, padfY,
-                                 padfZ, padfM);
+		SHPHandle SHP;
+		DBFHandle DBF;
 
-      int shp_num = SHPWriteObject(SHP, -1, psObject);
-      if(type=="RIVER")
-        DBFWriteDoubleAttribute(DBF, shp_num, field_num, shape.volume);
-      if(type=="BLUEFIELD"){
-        DBFWriteDoubleAttribute(DBF, shp_num, field_num, shape.volume);
-        DBFWriteIntegerAttribute(DBF, shp_num, elevation_field_num, shape.elevation);
-        DBFWriteStringAttribute(DBF, shp_num, name_field_num, shape.name.c_str());
-      }
-      SHPDestroyObject(psObject);
-      delete[] padfY;
-      delete[] padfX;
-    }
-    SHPClose(SHP);
-    DBFClose(DBF);
+		if(first_file){
+			SHP = SHPCreate(convert_string(shpName), SHPT_POLYGON);
+			DBF = DBFCreate(convert_string(dbfName));
+		}
+		else{
+			SHP = SHPOpen(convert_string(shpName), "rb+");
+			DBF = DBFCreate(convert_string(output_location + "/" + str(gs) + "_temp_shapefile_tile.dbf"));
+		}		
+
+		int field_num=0, elevation_field_num=0, name_field_num =0;
+		if(type=="RIVER")
+			field_num = DBFAddField(DBF, convert_string("DIS_AV_CMS"), FTDouble, 10, 3);
+		if(type=="BLUEFIELD"){
+			field_num = DBFAddField(DBF, convert_string("Vol_total"), FTDouble, 10, 3);
+			elevation_field_num = DBFAddField(DBF, convert_string("Elevation"), FTInteger, 10, 0);
+			name_field_num = DBFAddField(DBF, convert_string("Lake_name"), FTString, 64, 0);
+		}
+		if (SHP == NULL) {
+			printf("Unable to create:%s\n",
+					convert_string(output_location + "/" + str(gs) + "_shapefile_tile.shp"));
+			exit(1);
+		}
+
+		for (uint ipolygon = 0; ipolygon < relevant_polygons->get(lat + 90, lon + 180).size();
+			ipolygon++) {
+		Shape shape = shapes[relevant_polygons->get(lat + 90, lon + 180)[ipolygon]];
+		int nVertices = shape.points.size();
+		int panParts[1] = {0};
+
+		double *padfX = new double[nVertices];
+		double *padfY = new double[nVertices];
+		double *padfZ = NULL, *padfM = NULL;
+
+		for (int i = 0; i < nVertices; i++) {
+			GeographicCoordinate temp =
+				shape.points[i];
+			padfX[i] = temp.lon;
+			padfY[i] = temp.lat;
+		}
+		SHPObject *psObject;
+		psObject = SHPCreateObject(SHAPE_TYPE, -1, 0, panParts, NULL, nVertices, padfX, padfY,
+									padfZ, padfM);
+
+		int shp_num = SHPWriteObject(SHP, -1, psObject);
+		if(type=="RIVER")
+			DBFWriteDoubleAttribute(DBF, shp_num, field_num, shape.volume);
+		if(type=="BLUEFIELD"){
+			DBFWriteDoubleAttribute(DBF, shp_num, field_num, shape.volume);
+			DBFWriteIntegerAttribute(DBF, shp_num, elevation_field_num, shape.elevation);
+			DBFWriteStringAttribute(DBF, shp_num, name_field_num, shape.name.c_str());
+		}
+		SHPDestroyObject(psObject);
+		delete[] padfY;
+		delete[] padfX;
+		}
+
+		if(!first_file){
+			DBFHandle oldDBFHandle = DBFOpen(convert_string(dbfName),"rb");
+			DBFCopy(oldDBFHandle, DBF);
+			// Delete old DBF file and rename new DBF file to old DBF file
+			DBFClose(oldDBFHandle);
+			remove(convert_string(dbfName));
+			rename(convert_string(output_location + "/" + str(gs) + "_temp_shapefile_tile.dbf"), convert_string(dbfName));
+		}
+
+		SHPClose(SHP);
+		DBFClose(DBF);
+	}
+	first_file = false;
   }
 
   delete to_keep;
